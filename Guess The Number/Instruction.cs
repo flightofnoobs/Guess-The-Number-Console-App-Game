830b348b770ac7edf14e88ae72832c8b31580911
flightofnoobs <christianaz12345@gmail.com>
2022-01-29 19:17:11 +0800

    Add project files.

---
+++ Guess The Number/Instruction.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+
+public class Instruction
+{
+	public void InstructionMethod()
+	{
+		var mainMenu = new MainMenu();
+		Console.Clear();
+		Console.WriteLine("Instructions");
+		Console.WriteLine("");
+		Console.WriteLine("Select the difficulty then simply guess my number :)))");
+		Console.WriteLine("");
+		Console.WriteLine("Press Any Key to Continue");
+		Console.ReadKey();
+	}
+}
---
+++ Guess The Number/MainMenu.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+public class MainMenu
+{
+    public void TheMainMenu()
+    {
+        var c2 = new Instruction();
+        var c1 = "na";
+        Console.Clear();
+        Console.WriteLine("Guess The Number Game"); // where game starts
+        Console.WriteLine("");
+        Console.WriteLine("(1) Play");
+        Console.WriteLine("(2) Help");
+        Console.WriteLine("(3) Exit");
+        char selection = Console.ReadKey().KeyChar;
+
+        if (selection == '1')
+        {
+
+        }
+
+        else if (selection == '2')
+        {
+            c2.InstructionMethod();
+        }
+
+        else if (selection == '3')
+        {
+
+        }
+
+        else
+        {
+
+        }
+
+    }
+}
